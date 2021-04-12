b40ab56462c1fa150a0c7c0f857c5aae3ab46ba3
BulatKZN <bulat.gayfullin.1995@mail.ru>
2021-04-12 16:41:53 +0300

    burger-menu

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
   
 const burgerEl = document.querySelector('.header-menu-block-burger');
 
+const mobileMenu = document.querySelector(".mobile-menu");
+
 burgerEl.addEventListener('click', function(){
     if(burgerEl.classList.contains("active")) {
         burgerEl.classList.remove("active");
+        mobileMenu.classList.remove("active");
     }else {
-        burgerEl.classList.add('active')
+        burgerEl.classList.add('active');
+        mobileMenu.classList.add('active');
     };
 })
+
